b0f071dfb4d4b2ac4be67796c3f5c8f038cf1061
Dimitar Nikolov <dimitar.nikolov@egt-bg.com>
2017-11-27 14:33:19 +0200

    Added templates for common and main makefiles

---
+++ src/templates/MakefileCommon.h
@@ -0,0 +1,59 @@
+#ifndef __MAKEFILE_COMMON__
+#define __MAKEFILE_COMMON__
+
+#include <string>
+
+//=====================================================================================================================================
+
+std::string strMakefileCommon = "\
+SHELL                = /bin/bash\n\
+\n\
+SCREEN_WIDTH         = $(shell tput cols)\n\
+\n\
+NO_COLOR             = \033[0m\n\
+OK_COLOR             = \033[01;32m\n\
+ERR_COLOR            = \033[01;31m\n\
+WARN_COLOR           = \033[01;33m\n\
+FILE_COLOR           = \033[01;36m\n\
+LABEL_COLOR          = \033[01;34m\n\
+\n\
+OK_STR               = $(OK_COLOR)[OK]$(NO_COLOR)\n\
+ERR_STR              = $(ERR_COLOR)[ERRORS]$(NO_COLOR)\n\
+WARN_STR             = $(WARN_COLOR)[WARNINGS]$(NO_COLOR)\n\
+FILE_STR             = $(FILE_COLOR)$<$(NO_COLOR)\n\
+EXE_STR              = $(FILE_COLOR)$@$(NO_COLOR)\n\
+\n\
+PADDING              = awk '{ printf \"%-33s %-10s\\n\",$$1, $$2; }'\n\
+\n\
+WARN_BORDER          = for (( i=0; i<$(SCREEN_WIDTH); i++)); do printf \"$(WARN_COLOR)═$(NO_COLOR)\"; done && printf \"\\n\"\n\
+ERR_BORDER           = for (( i=0; i<$(SCREEN_WIDTH); i++)); do printf \"$(ERR_COLOR)═$(NO_COLOR)\"; done && printf \"\\n\"\n\
+\n\
+ERR_MSG              = printf \"$(FILE_STR) $(ERR_STR)\\n\" | $(PADDING) && $(ERR_BORDER) && printf \"$(CMD)\\n$$LOG\\n\" && $(ERR_BORDER) && false\n\
+WARN_MSG             = printf \"$(FILE_STR) $(WARN_STR)\\n\" | $(PADDING) && $(WARN_BORDER) && printf \"$(CMD)\\n$$LOG\\n\" && $(WARN_BORDER)\n\
+OK_MSG               = printf \"$(FILE_STR) $(OK_STR)\\n\" | $(PADDING)\n\
+\n\
+ERR_MSG_EXE          = printf \"$(EXE_STR) $(ERR_STR)\\n\" | $(PADDING) && $(ERR_BORDER) && printf \"$(CMD)\\n$$LOG\\n\" && $(ERR_BORDER) && false\n\
+WARN_MSG_EXE         = printf \"$(EXE_STR) $(WARN_STR)\\n\" | $(PADDING) && $(WARN_BORDER) && printf \"$(CMD)\\n$$LOG\\n\" && $(WARN_BORDER)\n\
+OK_MSG_EXE           = printf \"$(EXE_STR) $(OK_STR)\\n\" | $(PADDING)\n\
+\n\
+PRINT                = LOG=$$($(CMD) 2>&1) ; if [ $$? -eq 1 ]; then $(ERR_MSG); elif [ \"$$LOG\" != \"\" ]; then $(WARN_MSG); else $(OK_MSG); fi;\n\
+\n\
+PRINT_EXE            = LOG=$$($(CMD) 2>&1) ; if [ $$? -eq 1 ]; then $(ERR_MSG_EXE); elif [ \"$$LOG\" != \"\" ]; then $(WARN_MSG_EXE); else $(OK_MSG_EXE); fi;\n\
+\n\
+PRINT_DEPTH          = for (( i=1; i<$(MAKELEVEL); i++)); do printf \"$(LABEL_COLOR)║  $(NO_COLOR)\"; done;\n\
+\n\
+PRINT_DEPTH_HEADER   = if [ $(MAKELEVEL) -gt 1 ]; then for (( i=2; i<$(MAKELEVEL); i++)); do printf \"$(LABEL_COLOR)║  $(NO_COLOR)\"; done && printf \"$(LABEL_COLOR)╠══$(NO_COLOR)\"; else printf \"$(LABEL_COLOR)╔$(NO_COLOR)\"; fi; if [ $(MAKELEVEL) -gt 1 ]; then printf \"$(LABEL_COLOR)╦$(NO_COLOR)\"; fi;\n\
+\n\
+LIST                 = $(foreach file, $(SRCS), $(PRINT_DEPTH)printf \"$(LABEL_COLOR)╠═$(NO_COLOR)$(FILE_COLOR)$(file)$(NO_COLOR)\\n\";)\n\
+\n\
+CC = g++\n\
+\n\
+COMPILER_FLAGS       = -Wall -Wextra -fdiagnostics-color -g";
+
+//=====================================================================================================================================
+
+
+
+//=====================================================================================================================================
+
+#endif //__MAKEFILE_COMMON__
---
+++ src/templates/MakefileMain.h
@@ -0,0 +1,126 @@
+#ifndef __MAKEFILE_MAIN__
+#define __MAKEFILE_MAIN__
+
+#include <string>
+
+std::string strMakefileMain_1 = "\
+export COMMON = $(PWD)/Makefile_Common\n\
+\n\
+include $(COMMON)\n\
+\n\
+.NOTPARALLEL :\n\
+\n\
+MAKEFLAGS += -Otarget\n\
+\n\
+SRCDIR = ";
+
+std::string strMakefileMain_2 = "\n\
+\n\
+export OBJ_DIR = $(PWD)/";
+
+std::string strMakefileMain_3 = "\n\
+\n\
+LINKER_FLAGS = ";
+
+std::string strMakefileMain_4 = "\n\
+\n\
+EXE = ";
+
+std::string strMakefileMain_5 = "\n\
+\n\
+# ============================================================================================\n\
+\n\
+.PHONY : all\n\
+all : $(OBJ_DIR) _submodules $(EXE)\n\
+\n\
+$(OBJ_DIR) :\n\
+	@mkdir $(OBJ_DIR)\n\
+\n\
+.PHONY : _submodules\n\
+_submodules :\n\
+	@$(MAKE) --no-print-directory -C $(SRCDIR)\n\
+\n\
+$(EXE) : CMD = $(CC) $(OBJ_DIR)/*.o $(LINKER_FLAGS) -o $(EXE)\n\
+\n\
+$(EXE) :\n\
+	@$(PRINT_DEPTH_HEADER)printf \"$(LABEL_COLOR)═══[Linking executable]════$(NO_COLOR)\\n\"\n\
+	@$(PRINT_DEPTH)printf \"$(LABEL_COLOR)╠═$(NO_COLOR)\";$(PRINT_EXE)\n\
+	@$(PRINT_DEPTH)printf \"$(LABEL_COLOR)╚══$(NO_COLOR)\\n\"\n\
+\n\
+# ============================================================================================\n\
+\n\
+.PHONY : help\n\
+help : TARGET_COLOR = \033[01;35m\n\
+\n\
+help :\n\
+	@printf \"$(TARGET_COLOR)\\\"all\\\" or leave blank$(LABEL_COLOR):\\n\\n\
+    1. Builds source files.\\n\\n\
+    2. If there is no executable, links one together.$(NO_COLOR)\\n\\n\"\n\
+	@printf \"$(TARGET_COLOR)\\\"link\\\"$(LABEL_COLOR):\\n\\n\
+    1. Deletes executable.\\n\\n\
+    2. Links executable.$(NO_COLOR)\\n\\n\"\n\
+	@printf \"$(TARGET_COLOR)\\\"relink\\\"$(LABEL_COLOR):\\n\\n\
+    1. Deletes executable.\\n\n\
+    2. Builds source files.\\n\n\
+    3. Links executable.$(NO_COLOR)\n\n\"\n\
+	@printf \"$(TARGET_COLOR)\\\"nolink\\\"$(LABEL_COLOR):\\n\n\
+    1. Builds source files.$(NO_COLOR)\n\n\"\n\
+	@printf \"$(TARGET_COLOR)\\\"tree\\\"$(LABEL_COLOR):\\n\n\
+    1. Print source tree.$(NO_COLOR)\\n\\n\"\n\
+	@printf \"$(TARGET_COLOR)\\\"clean\\\"$(LABEL_COLOR):\\n\n\
+    1. Deletes executable.\\n\n\
+    2. Deletes objects directory.$(NO_COLOR)\\n\\n\"\n\
+	@printf \"$(TARGET_COLOR)\\\"delete_executable\\\"$(LABEL_COLOR):\\n\n\
+    1. Deletes executable.$(NO_COLOR)\\n\\n\"\n\
+	@printf \"$(TARGET_COLOR)\\\"delete_objects\\\"$(LABEL_COLOR):\\n\n\
+    1. Deletes objects directory.$(NO_COLOR)\\n\\n\"\n\
+\n\
+# ============================================================================================\n\
+\n\
+.PHONY : link\n\
+link : _headerClean delete_executable _footerClean $(EXE)\n\
+\n\
+# ============================================================================================\n\
+\n\
+.PHONY : relink\n\
+relink : _headerClean delete_executable _footerClean all\n\
+\n\
+# ============================================================================================\n\
+\n\
+.PHONY : nolink\n\
+nolink : $(OBJ_DIR) _submodules\n\
+\n\
+# ============================================================================================\n\
+\n\
+.PHONY : tree\n\
+tree :\n\
+	@$(MAKE) --no-print-directory -C $(SRCDIR) tree\n\
+\n\
+# ============================================================================================\n\
+\n\
+.PHONY : clean\n\
+clean : _headerClean delete_executable delete_objects _footerClean\n\
+\n\
+.PHONY : _headerClean\n\
+_headerClean :\n\
+	@$(PRINT_DEPTH_HEADER)printf \"$(LABEL_COLOR)════════[Cleaning]═════════$(NO_COLOR)\\n\"\n\
+\n\
+.PHONY : delete_executable\n\
+delete_executable : CMD = rm $(EXE)\n\
+\n\
+delete_executable :\n\
+	@-$(PRINT_DEPTH)printf \"$(LABEL_COLOR)╠═$(NO_COLOR)\";$(PRINT_EXE)\n\
+\n\
+.PHONY : delete_objects\n\
+delete_objects : CMD = rm -r $(OBJ_DIR)\n\
+\n\
+delete_objects : $(OBJ_DIR)\n\
+	@$(PRINT_DEPTH)printf \"$(LABEL_COLOR)╠═$(NO_COLOR)\";$(PRINT_EXE)\n\
+\n\
+.PHONY : _footerClean\n\
+_footerClean :\n\
+	@$(PRINT_DEPTH)printf \"$(LABEL_COLOR)╚══$(NO_COLOR)\\n\"\n\
+\n\
+# ============================================================================================";
+
+#endif //__MAKEFILE_MAIN__
